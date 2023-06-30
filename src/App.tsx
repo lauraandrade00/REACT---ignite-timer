b1ed714f336ef6ade28b4986f3a29e1ec5be15d8
Laura Andrade <lauraandradecamp@gmail.com>
2023-06-30 19:10:55 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button color="primary" />
+      <Button color="secondary" />
+      <Button color="success" />
+      <Button color="danger" />
+      <Button />
+    </ThemeProvider>
+  )
+}

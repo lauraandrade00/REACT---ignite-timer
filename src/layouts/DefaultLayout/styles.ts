e349f75a0fbd3074859f9816fa8f87b917693149
Laura Andrade <lauraandradecamp@gmail.com>
2023-07-05 16:24:56 -0300

    History's table done

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;  
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
-  padding: 2.5rem;
+  max-width: 60rem;  
+  height: calc(100vh - 1rem);
+  margin: 1rem auto;
+  padding: 1.5rem;
 
   background: ${props => props.theme["gray-800"]};
   border-radius: 8px;
